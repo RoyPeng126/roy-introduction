1746a095242b2d78aa93a2f457a13941c1b151e5
Roy Peng <roypengsan@gmail.com>
2025-12-13 13:44:52 +0800

    修正履歷顯示問題

--- src/main.js
+++ src/main.js
@@ -3,4 +3,14 @@ import App from './App.vue'
 import router from './router/index.js'   // 指到檔案
 import './assets/app.css'                // 你的專案是 app.css
 
-createApp(App).use(router).mount('#app')
+const app = createApp(App)
+
+app.use(router)
+
+const redirectPath = sessionStorage.getItem('redirectPath')
+if (redirectPath) {
+  sessionStorage.removeItem('redirectPath')
+  router.replace(redirectPath)
+}
+
+router.isReady().then(() => app.mount('#app'))
